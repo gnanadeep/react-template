eccdd63d3e3f250c4ea764d449fbab55c65d4bc8
gnanadeep <gnanadeep4@gmail.com>
2023-07-18 19:17:59 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Button from './Button/Button'
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <Button />
+    </>
+  )
+}
+
+export default App
